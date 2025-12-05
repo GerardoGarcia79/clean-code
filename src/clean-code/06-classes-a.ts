902c11400b0d1670ecf9b34c98b6ff6f7e61c513
Gerardo de Leon Garcia <gerardodlg.dev@gmail.com>
2025-12-05 13:36:50 -0600

    CLASS - Introduction to classes

---
+++ src/clean-code/06-classes-a.ts
@@ -0,0 +1,26 @@
+(() => {
+  type Gender = "M" | "F";
+  // Short way to declare a class
+  class Person {
+    constructor(
+      public name: string,
+      public gender: Gender,
+      public birthdate: Date
+    ) {}
+  }
+  // Long way to declare a class
+  // class Person {
+  //   public name: string;
+  //   public gender: Gender;
+  //   public birthdate: Date;
+
+  //   constructor(name: string, gender: Gender, birthdate: Date) {
+  //     this.name = name;
+  //     this.gender = gender;
+  //     this.birthdate = birthdate;
+  //   }
+  // }
+
+  const newPerson = new Person("Fernando", "M", new Date("1985-10-21"));
+  console.log({ newPerson });
+})();
