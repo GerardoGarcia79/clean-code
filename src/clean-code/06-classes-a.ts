517eed9741e0712971f097e73fa47e1d0fa217ac
Gerardo de Leon Garcia <gerardodlg.dev@gmail.com>
2025-12-05 14:22:29 -0600

    CLASS - Send objects in classes to extract the properties in the instance of the class

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,6 +1,8 @@
 (() => {
+  // No aplicando el principio de responsabilidad única
+
   type Gender = "M" | "F";
-  // Short way to declare a class
+
   class Person {
     constructor(
       public name: string,
@@ -8,19 +10,49 @@
       public birthdate: Date
     ) {}
   }
-  // Long way to declare a class
-  // class Person {
-  //   public name: string;
-  //   public gender: Gender;
-  //   public birthdate: Date;
 
-  //   constructor(name: string, gender: Gender, birthdate: Date) {
-  //     this.name = name;
-  //     this.gender = gender;
-  //     this.birthdate = birthdate;
-  //   }
-  // }
+  class User extends Person {
+    public lastAccess: Date;
 
-  const newPerson = new Person("Fernando", "M", new Date("1985-10-21"));
-  console.log({ newPerson });
+    constructor(
+      public email: string,
+      public role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(name, gender, birthdate);
+      this.lastAccess = new Date();
+    }
+
+    checkCredentials() {
+      return true;
+    }
+  }
+
+  class UserSettings extends User {
+    constructor(
+      public workingDirectory: string,
+      public lastOpenFolder: string,
+      email: string,
+      role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(email, role, name, gender, birthdate);
+    }
+  }
+
+  const userSettings = new UserSettings(
+    "/usr/home",
+    "/home",
+    "fernando@google.com",
+    "Admin",
+    "Fernando",
+    "M",
+    new Date("1985-10-21")
+  );
+
+  console.log({ userSettings });
 })();
